c2d5c1554930d3983b0703548acc866d8396e69b
Joseph50079 <josephogbah2019@gmail.com>
2024-11-13 17:38:36 +0100

    AUTH

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,18 +3,25 @@
 """ Module of Auth views
 """
 
-from flask import jsonify, request, abort
+from flask import json, request, abort
+from typing import List, TypeVar
+
 
 class Auth:
     """ Authentiction class
     """
-    
-    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
         """
         return None

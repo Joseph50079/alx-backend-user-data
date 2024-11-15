dbcf09d33b64a731981876501329193302f32651
Joseph50079 <josephogbah2019@gmail.com>
2024-11-15 15:18:03 +0100

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """ Module of Auth views"""
 
 from .auth import Auth
+from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+        # b_encoded = authorization_header[6:].encode('utf-8')
+        # return base64.b64encode(b_encoded)
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode_base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None

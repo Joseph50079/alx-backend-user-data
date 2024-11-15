8861903f474c3385492a79fc798d60ddd7046c3b
Joseph50079 <josephogbah2019@gmail.com>
2024-11-15 14:24:16 +0100

    basic auth implement

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,7 +20,11 @@ auth = getenv('AUTH_TYPE')
 
 if auth:
     from api.v1.auth.auth import Auth
-    auth = Auth()
+    from api.v1.auth.basic_auth import BasicAuth
+    if auth == 'basic_auth':
+        auth = BasicAuth()
+    else:
+        auth = Auth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" Module of Auth views"""
+
+from .auth import Auth
+
+class BasicAuth(Auth):
+    """ BasicAuth class
+
+    """
+    pass

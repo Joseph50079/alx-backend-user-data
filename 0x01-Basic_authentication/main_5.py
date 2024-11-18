9f3efb0abb9135cc6f52aaa27af05f355b01ca51
Joseph50079 <josephogbah2019@gmail.com>
2024-11-18 12:38:33 +0100

    Ok

--- 0x01-Basic_authentication/main_5.py
+++ 0x01-Basic_authentication/main_5.py
@@ -35,3 +35,17 @@ print(u.display_name() if u is not None else "None")
 u = a.user_object_from_credentials(user_email, user_clear_pwd)
 print(u.display_name() if u is not None else "None")
 
+#!/usr/bin/python3
+""" Check response
+"""
+
+if __name__ == "__main__":
+    from api.v1.auth.basic_auth import BasicAuth
+
+    ba = BasicAuth()
+    res = ba.user_object_from_credentials("u1@gmail.com", "pwd")
+    if res is not None:
+        print("user_object_from_credentials must return None if 'user_email' is not linked to any user")
+        exit(1)
+    
+    print("OK", end="")
